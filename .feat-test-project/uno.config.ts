f601cfd8537fb7285ceb65f953a5eeb33023cb27
VasFruzsina <vs.frzsn@gmail.com>
2025-05-08 15:00:06 +0200

    uno configuration, main colors declaration

---
+++ .feat-test-project/uno.config.ts
@@ -0,0 +1,40 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetWebFonts,
+  presetWind3,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
+      background: '#FFE6C9',
+      primary: '#73114B',
+      secondary: '#7F265B',
+      title: '#525252',
+      title2: '#828282',
+      inputfield: '#DED2D9',
+      placeholder: '#E0E0E0',
+      muted: '#A1A1A1',
+    },
+  },
+  presets: [
+    presetWind3(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        // ...
+      },
+    }),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+})
